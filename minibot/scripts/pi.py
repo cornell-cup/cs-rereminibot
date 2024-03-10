35be1b5eb98e9fcb6357a4c6bfcf771ddf9fbac0
Sanjana679 <sanjana@nandi.net>
2024-03-10 14:14:09 -0400

    Added structure for receiving uart mesages, running an LCD method, and sending an OK back
    Signed-off-by: Sanjana679 <sanjana@nandi.net>

--- minibot/scripts/pi.py
+++ minibot/scripts/pi.py
@@ -5,8 +5,23 @@
 #               try multiple times? Could cause a block on the RPi 0 side
 #    nonblocking - set so UART tries once and then runs update for LCD screen 
 
+import serial
+# timeout=None means there is no timeout between messages
+# xonoff is software control for 
+ser = serial.Serial('/dev/ttyAMA0', baudrate=115200, timeout=None, xonxoff=True)
+
 while True: 
     # Run UART receive, nonblocking 
-    # Run LCD methods
+    # request = 
 
+    # un-format request
+    if validate_crc_message(request, data_len=22):
+        request = unpack_crc_message(request)
+
+    if request == "": # some key that represents an animation LCD[emotion]
+        # Run LCD methods
+
+
+        # Send OK message back
+        ser.write(b'OK')
 
