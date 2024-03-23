b140d7fe2d32a27a5f025b340b71707c46db9fd5
John <jlw387@cornell.edu>
2024-03-23 13:52:55 -0400

    Animations can be played (WIP; STILL BUGGED)

--- static/gui/static/js/main.js
+++ static/gui/static/js/main.js
@@ -1,6 +1,6 @@
 /* ES6 */
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { nanoid } from 'nanoid';
 import ReactDOM from 'react-dom';
 import { CookiesProvider } from 'react-cookie';
@@ -15,6 +15,7 @@ import {
 // Minibot components import
 import Navbar from './components/Navbar.js';
 import Platform from './components/Platform.js';
+import * as expr_funcs from './components/Expression/ExpressionAnimation.js';
 import Chatbot from './components/Chatbot/chatbot2.js';
 
 // Utils import
@@ -91,6 +92,29 @@ function ClientGUI({ }) {
   }, [botVoiceControlMic, chatbotMic, changedMic])
   /****************************************************************************/
 
+  /**
+   ************************* EXPRESSION MANAGEMENT ****************************
+   * We use these props to manipulate the basestation's representation of the
+   * minibot's currently active expression.
+   ****************************************************************************
+   */
+
+  // Use useRef to create a reference to the sprite element
+  const expressionRef = useRef(null);
+
+  useEffect(() => {
+    if(expressionRef.current){
+      try{
+        expr_funcs.setAnimation("excited", 15);
+      }
+      catch(error)
+      {
+        console.error('An error occurred while running setAnimation():', error.message);
+      }
+    }
+  }, [expressionRef.current]); // Empty dependency array ensures the effect runs only once after mount
+
+  /****************************************************************************/
 
   useEffect(() => {
     window.addEventListener("beforeunload", alertUser);
@@ -124,6 +148,8 @@ function ClientGUI({ }) {
 
             contextHistoryLoaded={contextHistoryLoaded}
             setContextHistoryLoaded={setContextHistoryLoaded}
+
+            expressionRef={expressionRef}
           />
         </div>
         <Chatbot
