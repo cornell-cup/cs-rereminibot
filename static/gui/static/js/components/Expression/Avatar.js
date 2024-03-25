a4affd3931c60430cf92ad5731b3ff0bb5c951dc
John <jlw387@cornell.edu>
2024-03-25 12:53:09 -0400

    Reworked expression animations to use canvas rendering instead of CSS

---
+++ static/gui/static/js/components/Expression/Avatar.js
@@ -0,0 +1,166 @@
+import { SpritesheetJS } from "./Spritesheet.js"
+
+const relativePathToExpressionsImgFolder = "./static/img/Expressions/";
+const relativePathToExpressionsJSFolder = "./static/js/components/Expression/";
+
+export class AvatarJS
+{
+  
+  constructor(expressions = {}, currentExpression = null, currentPlaybackSpeed = 10.0, autoSaveExpressions = false) {
+    this.expressions = expressions;
+    this.jsonExpressionsDict = {};
+    this.currentExpression = null;
+    this.currentPlaybackSpeed = currentPlaybackSpeed;
+    this.currentFrame = 0.0;
+    this.autoSaveExpressions = autoSaveExpressions;
+
+    let setArbitraryStartingExpression = Object.keys(expressions).length > 0;
+
+    if (currentExpression !== null) {
+        if (expressions.hasOwnProperty(currentExpression)) {
+            this.currentExpression = currentExpression;
+            setArbitraryStartingExpression = false;
+        }
+    }
+
+    if (setArbitraryStartingExpression) {
+        this.currentExpression = Object.keys(expressions)[0];
+    }
+
+    this.prevUpdateTime = Date.now();
+  }
+
+  addOrUpdateExpression(expressionName, expressionSpritesheet) {
+    this.expressions[expressionName] = expressionSpritesheet;
+    this.jsonExpressionsDict[expressionName] = {
+        frameWidth: expressionSpritesheet.frameWidth,
+        frameHeight: expressionSpritesheet.frameHeight,
+        frameCount: expressionSpritesheet.frameCount,
+        sheetSrc: expressionSpritesheet.imageSrc
+    };
+    if (this.autoSaveExpressions) {
+        this.saveExpressionsJson(relativePathToExpressionsJSFolder + "expressions_autosave.json");
+    }
+  }
+
+  async loadExpressionsJson(json_src = relativePathToExpressionsJSFolder + "expressions.json", 
+                      img_dir = relativePathToExpressionsImgFolder) {
+    try {
+        console.log(json_src);
+        
+        const response = await fetch(json_src);
+        const jsonData = await response.json();
+
+        for (const key in jsonData) {
+            const sheet = new SpritesheetJS(
+                img_dir + jsonData[key].sheet_src,
+                jsonData[key].frame_width,
+                jsonData[key].frame_height,
+                jsonData[key].frame_count
+            );
+            console.log(`Created Spritesheet for ${key}`);
+            this.addOrUpdateExpression(key, sheet);
+        }
+        return true;
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
+  }
+
+  saveExpressionsJson(path) {
+    try {
+        const fs = require('fs');
+        fs.writeFileSync(path, JSON.stringify(this.jsonExpressionsDict, null, 2));
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+  }
+
+  validCurrentExpression() {
+    const expressionNotPresent = !this.expressions.hasOwnProperty(this.currentExpression);
+    const expressionNull = this.currentExpression === null;
+    
+    if(expressionNull || expressionNotPresent)
+        return false;
+
+    return this.expressions[this.currentExpression].loadedCorrectly;
+  }
+
+  getExpression(expressionName) {
+    return this.expressions[expressionName] || null;
+  }
+
+  setCurrentExpression(expressionName) {
+    if (expressionName === this.currentExpression) {
+      return true;
+    }
+    if (expressionName === null || expressionName === "") {
+      console.log("Null/Empty Expression Name");
+      return false;
+    }
+    if (this.expressions.hasOwnProperty(expressionName)) {
+      this.currentExpression = expressionName;
+      this.currentFrame = 0;
+      return true;
+    }
+    return false;
+  }
+
+  setPlaybackSpeed(newSpeed) {
+    this.currentPlaybackSpeed = newSpeed;
+  }
+
+  update() {
+    // Check to see if current expression is valid
+    if(!this.validCurrentExpression()){
+      // console.log("Invalid Expression Found During Update");
+      // console.log(`Expressions: ${this.expressions}`) ;
+      // console.log(`Current Expression: ${this.currentExpression}`); 
+      return;
+    }
+
+    // Get time since last frame
+    const currentTime = Date.now();
+    const deltaTime = (currentTime - this.prevUpdateTime) / 1000;
+    
+    // Update previous frame time variable
+    this.prevUpdateTime = currentTime;
+
+    // Compute temporal position of new frame (frame index but with decimals)
+    this.currentFrame += deltaTime * this.currentPlaybackSpeed;
+
+    // Keep the temporal position with the range [0, total number of frames)
+    while (this.currentFrame >= this.currentExpression.frameCount) {
+        this.currentFrame -= this.currentExpression.frameCount;
+    }
+
+    while (this.currentFrame < 0) {
+        this.currentFrame += this.currentExpression.frameCount;
+    }
+  }
+
+  drawCurrentDisplay(canvas) {
+    if(!this.validCurrentExpression())
+        return false;
+
+    return this.expressions[this.currentExpression].drawFrame(this.currentFrame, canvas);
+  }
+
+  getCurrentFrameCount() {
+    if(!this.validCurrentExpression())
+        return 0;
+    return this.expressions[this.currentExpression].frameCount;
+  }
+
+  getExpressionNames() {
+      return Object.keys(this.expressions);
+  }
+
+}
+
+const basestationAvatar = new AvatarJS();
+
+export {basestationAvatar};
---
+++ static/gui/static/js/components/Expression/Spritesheet.js
@@ -0,0 +1,53 @@
+export class SpritesheetJS{
+    constructor(src, frameWidth, frameHeight, frameCount) {
+        this.loadedCorrectly = false;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.frameCount = frameCount;
+        this.imageSrc = src;
+
+        console.log(`Image Src: ${src}`);
+
+        this.loaded = new Promise((resolve, reject) => {
+            this.fullSpritesheet = new Image();
+            this.fullSpritesheet.src = src;
+            this.fullSpritesheet.onload = () => {
+                this.loadedCorrectly = true;
+                resolve();
+            };
+            this.fullSpritesheet.onerror = () => {
+                this.loadedCorrectly = false;
+                reject(new Error('Failed to load spritesheet'));
+              };
+        });
+    }
+
+    /**
+     * Draw the specified frame on the specified canvas.
+     * @param {number} frameNumber - The number of the frame to draw.
+     * @param {number} canvas - The canvas element on which to draw the frame.
+     * @returns {boolean} Whether the frame was successfully drawn on the canvas.
+     */
+    drawFrame(frameNumber, canvas) {
+        if (!this.loadedCorrectly || !canvas)
+            return false;
+    
+        const context = canvas.getContext('2d');
+        while (frameNumber >= this.frameCount) {
+            frameNumber -= this.frameCount;
+        }
+        while (frameNumber < 0) {
+            frameNumber += this.frameCount;
+        }
+
+        const frameIndex = Math.floor(frameNumber);
+        const numFramesH = Math.floor(this.fullSpritesheet.width / this.frameWidth);
+        const row = Math.floor(frameIndex / numFramesH);
+        const col = frameIndex % numFramesH;
+        const sx = col * this.frameWidth;
+        const sy = row * this.frameHeight;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(this.fullSpritesheet, sx, sy, this.frameWidth, this.frameHeight, 0, 0, canvas.width, canvas.height);
+        return true;
+    }
+}
