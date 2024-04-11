18346c11701fca6b3136fd0f40678b3922bd93ab
Sanjana679 <sanjana@nandi.net>
2024-04-10 23:11:38 -0400

    Rewrote message_utils for UART; set up pi.py for receiving UART messages
    Signed-off-by: Sanjana679 <sanjana@nandi.net>

--- minibot/scripts/pi.py
+++ minibot/scripts/pi.py
@@ -1,27 +1,22 @@
-# Needs to run LCD screen
-# accept UART messages
-#    thread - can't communicate with main thread 
-#    blocking - only try once? Could cause a backlog on the Pico side 
-#               try multiple times? Could cause a block on the RPi 0 side
-#    nonblocking - set so UART tries once and then runs update for LCD screen 
-
 import serial
 # timeout=None means there is no timeout between messages
 # xonoff is software control for 
-ser = serial.Serial('/dev/ttyAMA0', baudrate=115200, timeout=None, xonxoff=True)
+ser = serial.Serial('/dev/ttyAMA0', baudrate=115200, timeout=0, stopbits=serial.STOPBITS_ONE)
 
 while True: 
     # Run UART receive, nonblocking 
-    # request = 
+    request = ser.readline().decode() # Decoding gets rid of newline character at the end 
 
     # un-format request
-    if validate_crc_message(request, data_len=22):
+    if request == 'Write.':
+        pass
+    elif validate_crc_message(request):
         request = unpack_crc_message(request)
 
-    if request == "": # some key that represents an animation LCD[emotion]
-        # Run LCD methods
+        if request == "": # some key that represents an animation LCD[emotion]
+            # Run LCD methods
 
 
-        # Send OK message back
-        ser.write(b'OK')
+            # Send OK message back
+            # ser.write(b'OK')
 
