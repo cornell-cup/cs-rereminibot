248254127dfb34277825a9482a8ed9e29d60630b
John <jlw387@cornell.edu>
2024-04-16 18:49:28 -0400

    Reworked Emotion Action Steps to Fix String Bugs

--- basestation/emotion_bs.py
+++ basestation/emotion_bs.py
@@ -9,26 +9,15 @@ def init_emotional_system(program):
     program.append("devices_emotional_status[\"display\"] = True\n")
     program.append("devices_emotional_status[\"speaker\"] = True\n")
 
-def create_action_steps_function(statements, exec_dict):
-    def action_function():
-        exec(statements, exec_dict)
-
-    return action_function
-
 class Emotion:
     """
     Represents an emotion that can be added to and run by an XRP bot.
     """
 
-    def __init__(self, name : str, action_steps, basestation, bot_script):
+    def __init__(self, name : str, action_steps):
         self._name = name
         self.required_devices = []
-        action_step_exec_dict = {
-            "bot_script" : bot_script,
-            "self" : basestation,
-            "time" : time
-        }
-        self.action_steps = create_action_steps_function(action_steps, action_step_exec_dict)
+        self.action_steps = action_steps
 
     def add_required_device(self, device : str):
         self.required_devices.append(device)
