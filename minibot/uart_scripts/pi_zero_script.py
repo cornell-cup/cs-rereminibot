9cf00aef1707a7b95e0c73381fca7fd035f5576c
Sanjana679 <sanjana@nandi.net>
2024-10-10 13:18:12 -0400

    Changed pi_zero_script
    Signed-off-by: Sanjana679 <sanjana@nandi.net>

--- minibot/uart_scripts/pi_zero_script.py
+++ minibot/uart_scripts/pi_zero_script.py
@@ -52,6 +52,10 @@ def run_pi_zero(demo_expression : str = "excited"):
 
     # timeout=None means there is no timeout between messages
     # xonoff is software control for 
+
+    while not os.path.exists('/dev/ttyACM0'):
+        pass
+
     serial_connected = 0
     print(os.path.exists('/dev/ttyACM0'))
     if os.path.exists('/dev/ttyACM0'):
@@ -61,81 +65,75 @@ def run_pi_zero(demo_expression : str = "excited"):
 
     running = True
     while running:
-        if serial_connected == 1:
-            try:
-                # Handle UART Signals
+        try:
+            # Handle UART Signals
+            
+            # Run UART receive, nonblocking 
+            if ser.inWaiting() > 0:
+                message = ser.readline().decode()
+                if len(message) > 0:
+                    print(message)
+                if message.startswith("SPR"): # some key that represents an animation LCD[emotion]
+                    # Run LCD methods
+                    expression_name = message.split(",")[1].replace("\n", "").strip()
                 
-                # Run UART receive, nonblocking 
-                if ser.inWaiting() > 0:
-                    message = ser.readline().decode()
-                    if len(message) > 0:
-                        print(message)
-                    if message.startswith("SPR"): # some key that represents an animation LCD[emotion]
-                        # Run LCD methods
-                        expression_name = message.split(",")[1].replace("\n", "").strip()
-                    
-                        print("Expression Name: \'" + expression_name + "\'")
+                    print("Expression Name: \'" + expression_name + "\'")
 
-                        if expression_name == "":
-                            pi_ava.clear_current_expression()
-                        else:
-                            pi_ava.set_current_expression(expression_name)
+                    if expression_name == "":
+                        pi_ava.clear_current_expression()
+                    else:
+                        pi_ava.set_current_expression(expression_name)
 
-                    elif message.startswith("PBS"):
-                        playback_speed = message.split(",")[1]
+                elif message.startswith("PBS"):
+                    playback_speed = message.split(",")[1]
 
-                        pi_ava.set_playback_speed(float(playback_speed))
+                    pi_ava.set_playback_speed(float(playback_speed))
 
-                    # Send OK message back
-                    # ser.write(b'OK')
-                
+                # Send OK message back
+                # ser.write(b'OK')
+            
 
-                # Update avatar
-                pi_ava.update()
-                frame = pi_ava.get_current_display()
-                
+            # Update avatar
+            pi_ava.update()
+            frame = pi_ava.get_current_display()
+            
 
-                # Update Pygame Screen
-                if frame is None:
-                    frame_np = np.zeros((320,480))
-                else:
-                    frame_np = np.array(frame)
+            # Update Pygame Screen
+            if frame is None:
+                frame_np = np.zeros((320,480))
+            else:
+                frame_np = np.array(frame)
 
-                if len(frame_np.shape) == 2:  # Grayscale to RGB
-                    frame_np = np.stack([frame_np]*3, axis=-1)
-                elif frame_np.shape[2] == 4:  # RGBA to RGB
-                    frame_np = frame_np[:, :, :3]
-                frame_surface = pygame.surfarray.make_surface(frame_np.swapaxes(0,1))
-                frame_surface = pygame.transform.scale(frame_surface, (infoObject.current_w, infoObject.current_h))
-                # frame_surface = pygame.transform.scale(frame_surface, (50, 50)) # Used to test in windowed mode
-                screen.blit(frame_surface, (0, 0))
-                pygame.display.update()
-                
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
+            if len(frame_np.shape) == 2:  # Grayscale to RGB
+                frame_np = np.stack([frame_np]*3, axis=-1)
+            elif frame_np.shape[2] == 4:  # RGBA to RGB
+                frame_np = frame_np[:, :, :3]
+            frame_surface = pygame.surfarray.make_surface(frame_np.swapaxes(0,1))
+            frame_surface = pygame.transform.scale(frame_surface, (infoObject.current_w, infoObject.current_h))
+            # frame_surface = pygame.transform.scale(frame_surface, (50, 50)) # Used to test in windowed mode
+            screen.blit(frame_surface, (0, 0))
+            pygame.display.update()
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    running = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        print("escape pressed")
                         pygame.quit()
                         running = False
-
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_ESCAPE:
-                            print("escape pressed")
-                            pygame.quit()
-                            running = False
-                
-                time.sleep(0.005)  # Adjust as needed for framerate
-            except KeyboardInterrupt as ki:
-                running = False
-                continue
-            except Exception as e:
-                # running = False
-                print("Encountered Exception!")
-                print(type(e), e)
-                print(traceback.format_exc())
-        elif serial_connected == 0:
-            if os.path.exists('/dev/ttyACM0'):
-                ser = serial.Serial('/dev/ttyACM0', 115200)
-                serial_connected = 1
-                time.sleep(1)
+            
+            time.sleep(0.005)  # Adjust as needed for framerate
+        except KeyboardInterrupt as ki:
+            running = False
+            continue
+        except Exception as e:
+            running = False
+            print("Encountered Exception!")
+            print(type(e), e)
+            print(traceback.format_exc())
 
     print("Pi Zero Script Complete.")
     pygame.quit()
