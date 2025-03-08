070a6abf8502a0334dd9dfe1536e2e32a7820a08
Manu Iyer <manupikachu18@gmail.com>
2025-03-08 13:59:00 -0500

    Testing sound scripts for XRP

---
+++ minibot/sound_test.py
@@ -0,0 +1,11 @@
+import pygame 
+# Initialize 
+
+pygame.mixer.init() 
+# Load and play sound file (replace 'sound.wav' with your file) 
+pygame.mixer.music.load("pi \home\cs-rereminibot\static\gui\static\sound\drum.wav")
+pygame.mixer.music.play()
+ # Keep script running while audio plays 
+
+while pygame.mixer.music.get_busy(): 
+    pass
